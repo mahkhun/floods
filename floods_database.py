6ffaf9f0c34e78b568657db45f299a93a6d16f6a
mahkhun <91360336+mahkhun@users.noreply.github.com>
2021-12-24 22:05:44 +0800

    Added functionality for querying in the database.

--- floods_database.py
+++ floods_database.py
@@ -1,7 +1,14 @@
 import sqlite3
 
-def create_database(table_name = str):
-    """ Creates a floods_data.db """
+def create_database() -> None:
+    conn = sqlite3.connect("floods_data.db")
+    c = conn.cursor()
+    conn.commit()
+    conn.close()
+
+
+def create_table(table_name) -> None:
+    """ Creates a floods_data.db and a table specified by its region. """
     conn = sqlite3.connect("floods_data.db")
     c = conn.cursor()
     c.execute(f"""
@@ -19,29 +26,65 @@ def create_database(table_name = str):
     conn.close()
 
 
-def insert_database(table_name, date, region, location, main_infrastructure, damaged_infrastructure, water_level, image):
+def insert_table(table_name, date, region, location, main_infrastructure, damaged_infrastructure, water_level, image) -> None:
+    """ Inserts new data in floods_data.db on the table it is specified to be on. """ 
     conn = sqlite3.connect("floods_data.db")
     c = conn.cursor()
     # Image
     with open(image, "rb") as image_file:
         image = image_file.read()
     c.execute(f"""
-    INSERT INTO {table_name} (date, region, location, main_infrastructure, damaged_infrastructure, water_level, image) 
-    VALUES (?,?,?,?,?,?,?)""", (date, region, location, main_infrastructure, damaged_infrastructure, water_level, image))
+    INSERT INTO {table_name} (
+        date, 
+        region, 
+        location, 
+        main_infrastructure, 
+        damaged_infrastructure, 
+        water_level, 
+        image
+        ) 
+    VALUES (?,?,?,?,?,?,?)""", (
+        date, 
+        region, 
+        location, 
+        main_infrastructure, 
+        damaged_infrastructure, 
+        water_level, 
+        image
+        )
+    )
     conn.commit()
     conn.close()
 
-def query_database(table_name):
+def query_table(table_name, *columns, **filters) -> dict:
+    """ Gathers data from the specified table name from floods_data.db"""
     conn = sqlite3.connect("floods_data.db")
     c = conn.cursor()
-    c.execute(f"""
-    SELECT * FROM {table_name}
-    """)
-    result = c.fetchall()
-    print(result)
+    results = {column: [] for column in columns}
+    clauses = " ".join([f"{clause} {condition}" for clause, condition in filters.items()])
+    for column in columns:
+        c.execute(f"""
+        SELECT {column} FROM {table_name} {clauses}
+        """)
+        for data in c.fetchall():
+            results[column].append(data[0])
     conn.commit()
     conn.close()
+    return results
+
+# def query_table_list() -> None:
+#     conn = sqlite3.connect("floods_data.db")
+#     c = conn.cursor()
+#     c.execute(f"""
+#     SELECT name FROM sqlite_master WHERE type='table';
+#     """)
+#     table_list = [result[0] for result in c.fetchall()]
+#     # print(table_list)
+#     return table_list
 
 
 if __name__ == "__main__":
-    pass
+    # create_database()
+    print(query_table("sqlite_master", "name", WHERE="type='table'"))
+    # query_table("national_capital_region", "location", "water_level")
+    # print(query_table("national_capital_region", "date", WHERE="location=='Quezon City'"))
