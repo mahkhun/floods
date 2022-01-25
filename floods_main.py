337375018405908e8bb4375f861d115036912664
mahkhun <91360336+mahkhun@users.noreply.github.com>
2022-01-25 15:00:04 +0800

    random update

--- floods_main.py
+++ floods_main.py
@@ -20,11 +20,11 @@ with open("db-tablenames.json") as f:
 
 class FloodsMainInterface(object):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.show_home_ui()
 
 
-    def show_home_ui(self):
+    def show_home_ui(self) -> None:
         """ Displays the FloodsHome window. """
         self.home_ui = FloodsHomeInterface()
         self.home_ui.show()
@@ -32,7 +32,7 @@ class FloodsMainInterface(object):
         self.home_ui.open_selection_signal.connect(self.show_selection_ui)
 
 
-    def show_form_ui(self):
+    def show_form_ui(self) -> None:
         """ Displays the FloodsForm window. """
         self.form_ui = FloodsFormInterface()
         self.form_ui.show()
@@ -40,7 +40,7 @@ class FloodsMainInterface(object):
         self.form_ui.accomplished_form_signal.connect(self.show_form_dialog_ui)
 
     
-    def show_form_dialog_ui(self, data_dict):
+    def show_form_dialog_ui(self, data_dict) -> None:
         """ Displays the FloodsForm dialog window. """
         # insert data in floodsformdialog for proper init
         self.form_dialog_ui = FloodsFormDialog(data_dict)
@@ -49,7 +49,7 @@ class FloodsMainInterface(object):
         self.form_dialog_ui.create_new_signal.connect(self.show_form_ui)
 
 
-    def show_selection_ui(self):
+    def show_selection_ui(self) -> None:
         """ Displays the FloodsSelection window. """
         self.selection_ui = FloodsSelectionInterface()
         self.selection_ui.show()
@@ -61,7 +61,7 @@ class FloodsHomeInterface(qtw.QWidget, Ui_FloodsHome):
     open_form_signal = qtc.pyqtSignal()
     open_selection_signal = qtc.pyqtSignal()
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
@@ -70,16 +70,15 @@ class FloodsHomeInterface(qtw.QWidget, Ui_FloodsHome):
         self.view_button.clicked.connect(self.open_selection_window)
 
 
-    def open_form_window(self):
+    def open_form_window(self) -> None:
         """ Opens the FloodsForm window and closes the FloodsHome window. """
         self.close()
         self.open_form_signal.emit()
 
-    def open_selection_window(self):
+    def open_selection_window(self) -> None:
         """ Opens the FloodsSelection window and closes the FloodsHome window. """
         self.close()
         self.open_selection_signal.emit()
-        
 
 
 class FloodsFormInterface(qtw.QWidget, Ui_FloodsForm):
@@ -87,7 +86,7 @@ class FloodsFormInterface(qtw.QWidget, Ui_FloodsForm):
     cancel_form_signal = qtc.pyqtSignal()
     accomplished_form_signal = qtc.pyqtSignal(dict)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
@@ -102,19 +101,19 @@ class FloodsFormInterface(qtw.QWidget, Ui_FloodsForm):
         self.submit_button.clicked.connect(self.submit_form)
 
 
-    def close_form(self):
+    def close_form(self) -> None:
         """ Closes the FloodsForm window. """
         self.close()
         self.close_form_signal.emit()        
 
 
-    def accomplished_form(self, data_dict):
+    def accomplished_form(self, data_dict) -> None:
         """ Shows the FloodsFormDialog window. """
         self.close()
         self.accomplished_form_signal.emit(data_dict)
 
 
-    def update_location_combobox(self, index):
+    def update_location_combobox(self, index) -> None:
         """ Updates the location combobox with the location associated with the region selected in the region combobox. """
         self.location_combobox.clear()
         locations = self.region_combobox.itemData(index)
@@ -122,13 +121,13 @@ class FloodsFormInterface(qtw.QWidget, Ui_FloodsForm):
             self.location_combobox.addItems(locations)
 
 
-    def browse_files(self):
+    def browse_files(self) -> None:
         """ Allows the user to browse files. """
         file_name = qtw.QFileDialog.getOpenFileName(self, "Select Image File", str(Path.home()))
         self.image_lineedit.setText(file_name[0])
 
 
-    def submit_form(self):
+    def submit_form(self) -> None:
         """ Gathers data about floods. """
         form_dict = {
             "date": self.date_lineedit.text(),
@@ -158,10 +157,8 @@ class FloodsFormInterface(qtw.QWidget, Ui_FloodsForm):
                 form_dict["water level"],
                 form_dict["image"]
             )
-            # 
             self.accomplished_form(form_dict)
             
-
 
 class FloodsFormDialog(qtw.QDialog, Ui_FloodsFormDialog):
 
@@ -169,7 +166,7 @@ class FloodsFormDialog(qtw.QDialog, Ui_FloodsFormDialog):
     return_home_signal = qtc.pyqtSignal()
 
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.update_descriptions(args[0])
@@ -177,24 +174,25 @@ class FloodsFormDialog(qtw.QDialog, Ui_FloodsFormDialog):
         self.create_new_button.clicked.connect(self.create_new_form)
 
 
-    def update_descriptions(self, data_dict):
+    def update_descriptions(self, data_dict) -> None:
         """ Updates the description displayed in the FloodsFormDialog window. """
-        self.date_description_label.setText(data_dict["date"])
-        self.region_description_label.setText(data_dict["region"])
-        self.location_description_label.setText(data_dict["city/province"])
-        self.infrastructure_description_label.setText(data_dict["type of infrastructure"])
-        self.damage_description_label.setText(data_dict["extent of damage"])
-        self.waterlevel_description_label.setText(data_dict["water level"])
+        desc = "Description: {}"
+        self.date_description_label.setText(desc.format(data_dict["date"]))
+        self.region_description_label.setText(desc.format(data_dict["region"]))
+        self.location_description_label.setText(desc.format(data_dict["city/province"]))
+        self.infrastructure_description_label.setText(desc.format(data_dict["type of infrastructure"]))
+        self.damage_description_label.setText(desc.format(data_dict["extent of damage"]))
+        self.waterlevel_description_label.setText(desc.format(data_dict["water level"]))
         # - image update here
 
     
-    def create_new_form(self):
+    def create_new_form(self) -> None:
         """ Closes the FloodsFormDialog window and emits create_new_signal to signify opening another new FloodsForm window. """
         self.close()
         self.create_new_signal.emit()
 
 
-    def close_form_dialog(self):
+    def close_form_dialog(self) -> None:
         """ Closes the FloodsFormDialog window and emits the return_home_signal to signify to return in the FloodsHome window."""
         self.close()
         self.return_home_signal.emit()
@@ -204,7 +202,7 @@ class FloodsSelectionInterface(qtw.QWidget, Ui_FloodsSelection):
 
     close_selection_signal = qtc.pyqtSignal()
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         # Initializes and sets up the FloodsSelection window.
         self.setupUi(self)
@@ -228,7 +226,7 @@ class FloodsSelectionInterface(qtw.QWidget, Ui_FloodsSelection):
         self.cancel_button.clicked.connect(self.close_selection)
 
 
-    def close_selection(self):
+    def close_selection(self) -> None:
         """ Closes the FloodsSelection window. """
         # Closes the FloodsSelecton window.
         self.close()
@@ -237,7 +235,7 @@ class FloodsSelectionInterface(qtw.QWidget, Ui_FloodsSelection):
         self.close_selection_signal.emit()
 
         
-    def update_region_combobox(self):
+    def update_region_combobox(self) -> None:
         """ Updates the region combobox with the tables found in floods_data.db along with the available corresponding cities."""
         # Queries a list of table names from floods_data.db.
         table_list = db.query_table("sqlite_master", "name", WHERE="type='table'")["name"]
@@ -258,7 +256,7 @@ class FloodsSelectionInterface(qtw.QWidget, Ui_FloodsSelection):
             self.region_combobox.addItem(region, location)
 
     
-    def update_location_combobox(self, region, index):
+    def update_location_combobox(self, region, index) -> None:
         """ Updates the location combobox with the cities/provinces associated with the current region presented in the region combobox."""
         # Clears the items stored in the location combobox.
         self.location_combobox.clear()
@@ -272,7 +270,7 @@ class FloodsSelectionInterface(qtw.QWidget, Ui_FloodsSelection):
             self.location_combobox.addItem(location, date_list)
 
 
-    def update_date_combobox(self, index):
+    def update_date_combobox(self, index) -> None:
         """ Updates the date combobox with the dates associated with the current city/province presented in the location combobox. """
         # Clears the items stored in the date combobox.
         self.date_combobox.clear()
